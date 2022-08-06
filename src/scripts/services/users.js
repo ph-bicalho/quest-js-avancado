438ba261004e9bc1bae9d1338e9e68d014b48ddf
Pedro Bicalho <pedrohenriquebicalho@hotmail.com>
2022-08-06 11:09:13 -0300

    fix nos imports

--- src/scripts/services/users.js
+++ src/scripts/services/users.js
@@ -1,4 +1,4 @@
-import {baseUrl} from  '/src/scripts/variables.js'
+import {baseUrl} from  'variables.js'
 
 let getUser= async function(userName){
     const response = await fetch(`${baseUrl}/${userName}`)
